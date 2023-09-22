2e47856f5c4c56e89c4c5f70d733ce86e011909c
Jaloliddin <bosimovjaloliddin@gmail.com>
2023-09-22 15:14:15 +0500

    add popular tools

--- src/components/Category/PopularTools/index.jsx
+++ src/components/Category/PopularTools/index.jsx
@@ -1,5 +1,7 @@
 import {
   Container,
+  Keyword,
+  KeywordTitle,
   Title,
   Tool,
   ToolSubtitle,
@@ -11,31 +13,42 @@ const PopularTools = () => {
   return (
     <Container className="container">
       <Title>Popular tools</Title>
-      <Wrapper>
+      <Wrapper $jc="space-between" $mt={24}>
         <Tool>
           <ToolTitle>HTML 5</ToolTitle>
           <ToolSubtitle>2,736 Courses</ToolSubtitle>
         </Tool>
         <Tool>
-          <ToolTitle>HTML 5</ToolTitle>
+          <ToolTitle>CSS</ToolTitle>
           <ToolSubtitle>2,736 Courses</ToolSubtitle>
         </Tool>
         <Tool>
-          <ToolTitle>HTML 5</ToolTitle>
+          <ToolTitle>JavaScropt</ToolTitle>
           <ToolSubtitle>2,736 Courses</ToolSubtitle>
         </Tool>
         <Tool>
-          <ToolTitle>HTML 5</ToolTitle>
+          <ToolTitle>SaaS</ToolTitle>
           <ToolSubtitle>2,736 Courses</ToolSubtitle>
         </Tool>
         <Tool>
-          <ToolTitle>HTML 5</ToolTitle>
+          <ToolTitle>Larvel</ToolTitle>
           <ToolSubtitle>2,736 Courses</ToolSubtitle>
         </Tool>
         <Tool>
-          <ToolTitle>HTML 5</ToolTitle>
+          <ToolTitle>Django</ToolTitle>
           <ToolSubtitle>2,736 Courses</ToolSubtitle>
         </Tool>
+      </Wrapper>
+      <Wrapper $jc="flex-start" $mt={40}>
+        <KeywordTitle>Popular keyword:</KeywordTitle>
+        <Keyword type="button">HTML 5</Keyword>
+        <Keyword type="button">Web Development</Keyword>
+        <Keyword type="button">Responsive Developments</Keyword>
+        <Keyword type="button">Developments</Keyword>
+        <Keyword type="button">Programing</Keyword>
+        <Keyword type="button">Website</Keyword>
+        <Keyword type="button">Technology</Keyword>
+        <Keyword type="button">Wordpress</Keyword>
       </Wrapper>
     </Container>
   );
--- src/components/Category/PopularTools/style.js
+++ src/components/Category/PopularTools/style.js
@@ -12,9 +12,10 @@ const Title = styled.h3`
 const Wrapper = styled.div`
   display: flex;
   flex-wrap: wrap;
-  justify-content: space-between;
+  justify-content: ${({ $jc }) => $jc && $jc};
+  align-items: center;
   gap: 2%;
-  margin-top: 24px;
+  margin-top: ${({ $mt }) => $mt && `${$mt}px`};
 `;
 const Tool = styled.div`
   flex: 0 0 15%;
@@ -54,5 +55,43 @@ const ToolSubtitle = styled.p`
   text-align: center;
   color: #8c94a3;
 `;
+const KeywordTitle = styled.h4`
+  font-size: 18px;
+  font-weight: 400;
+  line-height: 24px;
+  letter-spacing: -0.015em;
+  text-align: left;
+  color: #1d2026;
+`;
+const Keyword = styled.button`
+  font-size: 14px;
+  font-weight: 500;
+  line-height: 20px;
+  letter-spacing: -0.01em;
+  text-align: left;
+  padding: 6px 12px;
+  color: #1d2026;
+  background-color: #f5f7fa;
+  border: 0;
+  cursor: pointer;
+  transition: all 0.3s ease;
 
-export { Container, Title, Wrapper, Tool, ToolTitle, ToolSubtitle };
+  &:hover {
+    color: #ffffff;
+    background-color: #ff6636;
+  }
+  &:active {
+    transform: scale(0.96);
+  }
+`;
+
+export {
+  Container,
+  Title,
+  Wrapper,
+  Tool,
+  ToolTitle,
+  ToolSubtitle,
+  KeywordTitle,
+  Keyword,
+};
